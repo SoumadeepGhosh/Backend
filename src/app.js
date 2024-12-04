ff80b262061c7bc32c49d0f186ddebf5d0a9ccff
Soumadeep Ghosh <144361361+itzsoumadeep@users.noreply.github.com>
2024-12-04 09:40:35 +0530

    add register controler

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended:true , limit : "16kb"}))
-
-app.use(express.static("public"))
+app.use(express.static("public"));
 app.use(cookieParser());
-export { app }
+
+// import route
+import userRouter from "./routes/user.routes.js";
+
+// Route Declaration
+app.use("/api/v1/users", userRouter);
+
+export { app };

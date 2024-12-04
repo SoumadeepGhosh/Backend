1cee63a68994c7a12421c54d47056e62327fc2f0
Soumadeep Ghosh <144361361+itzsoumadeep@users.noreply.github.com>
2024-12-04 22:51:34 +0530

    fix all register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,20 +2,20 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
-const router = Router();
+const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
             name: "avatar",
             maxCount: 1
-        },
+        }, 
         {
-            name : "coverImage",
+            name: "coverImage",
             maxCount: 1
         }
     ]),
     registerUser
-    );
+    )
 
 export default router;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,27 +1,31 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
 
-// Configuration
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-  try {
-    if (!localFilePath) return null;
-    //uplode  the file on cloudinary
-    const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
-    });
-    //file has been uploded successfull
-    console.log("File is uploded on cloudinary..", response.url);
-    return response;
-  } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the localy saved tempory file as the uplode operation got failed.
-    return null;
-  }
-};
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
 
-export {uploadOnCloudinary};
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+
+export {uploadOnCloudinary}

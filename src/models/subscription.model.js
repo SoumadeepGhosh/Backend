9697643f79c8303b9dbd329cb0c1b3227d7dd3ed
Soumadeep Ghosh <144361361+itzsoumadeep@users.noreply.github.com>
2024-12-07 23:21:57 +0530

    Add watch history successfully

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,7 +4,7 @@ import mongoose, { Schema } from "mongoose";
 
 
 const subscriptionSchema =  new Schema ({
-    subcriber:{
+    subscriber:{
         type: Schema.Types.ObjectId, // => one who is being Subscribing
         ref: "User"
     },
